a22406807c432a2384425782f7cc6856e597fda5
Akerele-creator <akerele2021@gmail.com>
2024-08-07 04:44:28 -0700

    updated app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def load_data():
     df.rename(columns={'Unnamed: 0': 'id',
                    'article': 'product',
                    'Quantity': 'quantity'},
-                     inplace = True)
+                  inplace = True)
     df.unit_price = df.unit_price.str.replace(",",".").str.replace("€","").str.strip()
     df.unit_price = df.unit_price.astype('float')
     #calculate sales
@@ -32,14 +32,14 @@ st.title("Bakery Sales App")
 # select and display specific products
 # add filter
 
-products = df['products'].unique()
-selected_products = st.sidebar.multiselect(
+products = df["product"].unique()
+selected_product = st.sidebar.multiselect(
                     "Choose Product",
                     products,
                     [products[0],
-                    produts[2]
+                    products[2]
                     ])
-filtered_table = df[df['product'].isin(selected_products)]
+filtered_table = df[df['product'].isin(selected_product)]
 
 #display metrics
 # total_sales = 0
@@ -49,12 +49,12 @@ else:
     total_sales = df.sales.sum()
 
 total_qty = df.quantity.sum()
-total_no_tansaction = df.id.count()
+total_no_tansactions = df.id.count()
 
 st.subheader("Calculations")
 col1, col2, col3 = st.columns(3)
 
-col1.metric("No of transaction", total_no_tansaction)
+col1.metric("No of Transaction", total_no_tansactions)
 col2.metric("Total Quantity", total_qty)
 col3.metric("Total Sales", total_sales)
 
@@ -62,23 +62,34 @@ col3.metric("Total Sales", total_sales)
 
 #display the filtered table
 # specific columns
-st.dataframe(filtered_table[["date","products",
-                            "quantity","unit_price"
+st.dataframe(filtered_table[['date',"product",
+                            "quantity","unit_price",
                             "sales"]])
 # barchat
 try:
     st.write("## Total sales of selected products")
-    bar1 = filtered_table.groupby(['products'])['sales'].sum().sort_value(ascending=True)
+    bar1 = filtered_table.groupby(['product'])['sales'].sum().sort_values(ascending=True)
     st.bar_chart(bar1)
 except ValueError as e:
     st.error(
         """ Error: """ % e.reason
     )
  # sales Analysis
-daily_qty_sold = df.groupby('date')['quantity'].sum()
-daily_qty_sold_df = daily_qty_sold.reset_index().rename(columns={'quantity':"total qty"})
-ax = daily_sales_df.plot.area(x='date',
-                              y='total sales')
+try:
+    if len(filtered_table) > 0:
+        daily_sales = filtered_table.groupby('date')['sales'].sum()
+    else:
+         daily_sales = df.groupby('date')['sales'].sum()
+    daily_sales_df = daily_sales.reset_index().rename(columns={'sales':"total sales"})
+    ax = daily_sales_df.plot.area(x='date', 
+                                y='total sales')
+    st.area_chart(ax)(daily_sales_df,
+                    x = 'date',
+                    y = 'total sales')
+except ValueError as e:
+    st.error(
+          """ Error: """ % e.reason
+    )
 
 
 
